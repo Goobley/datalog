97638c9722a8b54762d9c901d022252af168e6df
cmo <c.osborne.1@research.gla.ac.uk>
2022-12-16 16:30:41 +0000

    bump version

--- datalog/__init__.py
+++ datalog/__init__.py
@@ -2,8 +2,8 @@
 
 import logging
 
-__author__ = "Sean Leavey <datalog@attackllama.com>"
-__version__ = "0.7.8"
+__author__ = "Sean Leavey <datalog@attackllama.com>/small changes by cmo <software@contextuallight.com>"
+__version__ = "0.7.9"
 
 # suppress warnings when the user code does not include a handler
 logging.getLogger().addHandler(logging.NullHandler())
